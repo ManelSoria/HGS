5375f7375b7139e4c6d7fc6c986b5f9ebd57ac80
Caleb_Fuster <calebfuji@gmail.com>
2020-12-16 16:26:08 +0100

    The MAN problem with RH

--- Examples/Ex10_The_MAN_problem.m
+++ Examples/Ex10_The_MAN_problem.m
@@ -6,37 +6,52 @@
 %***********************************************************************************************************
 %
 % Example 10: The MAN problem
-% Wet air reaction with hydrocarbons
+% Wet air reaction with hydrocarbons. Using Relative humidity.
+% At ambient pressure and temperature.
+
 clearvars
 close all
 clc
 
-hum = 0:2:100;
 
-for ii = 1:length(hum)
-    % Humidity ratio
-    x = hum(ii); % [%] x = Kgwater/Kgdryair *100
+RH = 0:2:100; % [%] Relative humidity
+T1 = 298.15; % [K] Air temperature
+T2 = 350; % [K] Fuel temperature
+Kgwetair = 1; % [kg] Wet air mass flow
+P = 1; % [bar] Atmospheric pressure
+V = 0.01; % [m^3] Volume
+R = 8.3144621; % [J / (mol*K)]
 
-    % Wet air in Kg
-    Kgwetair = 0.1; % [kg] Kgwetair = Kgwater + Kgdryair
-    Kgdryair = Kgwetair / (x/100 + 1); % [kg]
-    Kgwater = x / 100 * Kgdryair; % [kg]
+nwetair = P*10^5*V / (R*T1); % [mol]
 
+
+% Good approx up to 40ºC. Be carefull Tc (ºC)
+% http://hyperphysics.phy-astr.gsu.edu/hbase/Kinetic/relhum.html#c4
+VDsat = @(Tc) 5.018 + 0.32321*Tc + 8.1847*10^(-3)*Tc^2 +3.1243*10^(-4)*Tc^3;
+VDsatAmb = VDsat(T1-273.15);
+
+
+for ii = 1:length(RH)
+    % RH = VD / VDsat *100;
+    VD = RH(ii) * VDsatAmb / 100 ;
+    
+    % VD = KgH2O / KgH2 =  KgH2O / 2
+    KgH2O = VD * 2;
+        
     % Dry air composition in mols
     dryaircomp = [0.79 0.21];
-
+    
     % Molar mass
     Mmdryair = HGSprop({'N2' 'O2'},dryaircomp,[],[],'Mm');
-    Mmwater = HGSprop({'H2O'},1,[],[],'Mm');
+    MmH2O = HGSprop({'H2O'},1,[],[],'Mm');
+    
+    nH2O = KgH2O / MmH2O;
+            
+    ndryair  = nwetair - nH2O;
+    
+    % Wetair composition in mols
+    nwetaircomp = [dryaircomp(1)*ndryair dryaircomp(2)*ndryair  nH2O] / (ndryair + nH2O) ;
 
-    % Mols air
-    nwater = Kgwater * 1000 / Mmwater;
-    ndryair = Kgdryair * 1000 / Mmdryair;
-
-    nN2dry = dryaircomp(1) * ndryair;
-    nO2dry = dryaircomp(2) * ndryair;
-
-    nwetaircomp = [nN2dry nO2dry nwater] / (nN2dry + nO2dry + nwater);
 
     % Wet air
     wet = {'N2' 'O2' 'H2O'};
@@ -44,20 +59,20 @@ for ii = 1:length(hum)
     nwet =  nwetair * nwetaircomp; % [mols]
 
     % Fuels
-    fuel = {'CH4'};
-    nfuel = [0.4];  % [mols]
+    fuel = {'CH4' 'C2H6' 'C3H8'};
+    nfuel = [1.0 0.8 0.5];  % [mols]
 
     % Species
     % HGS functions requires all the species that you want to be calculated.
     % If you want to put dissociation, it is possible. Like in this example H2
     % and H or O2 and O.
 
-    species = {wet{1:end} fuel{1:end}  'CO2' };
-    n0  =     [   nwet        nfuel      0   ];
+    species = {wet{1:end} fuel{1:end}  'CO2'  'CO'};
+    n0  =     [   nwet        nfuel      0     0 ];
 
     % Suposing that Wet air and Fuels have not the same inlet T
-    Hwet = HGSprop(wet,nwet,298.15,1,'H');
-    Hfuels = HGSprop(fuel,nfuel,350,1,'H');
+    Hwet = HGSprop(wet,nwet,T1,1,'H');
+    Hfuels = HGSprop(fuel,nfuel,T2,1,'H');
 
     Hin = Hwet + Hfuels;
 
@@ -69,24 +84,26 @@ for ii = 1:length(hum)
 end
 
 figure
-plot(hum,Tp);
+plot(RH,Tp);
 ylabel('Tp [K]')
 xlabel('Humidity [%]')
 
 figure
 hold on
 for ii = 1 : length(species)
-    plot(hum,nper(:,ii)*100);
+    plot(RH,nper(:,ii)*100);
 end
 xlabel('Humidity [%]')
 ylabel('Mols [%]')
+axis([0 100 0 25])
 legend(species)
 
 figure
 hold on
 for ii = 1 : length(species)
-    plot(hum,n(:,ii));
+    plot(RH,n(:,ii));
 end
 xlabel('Humidity [%]')
 ylabel('Mols [mols]')
+axis([0 100 0 5])
 legend(species)
