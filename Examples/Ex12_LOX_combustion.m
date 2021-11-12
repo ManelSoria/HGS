48e6546d4d3d4fad01a52b1cc4eb7c97a11d8d22
ManelSoria <manel.soria@upc.edu>
2021-11-12 18:36:45 +0100

    Example 12: LOX LH2 combustion redone

--- Examples/Ex12_LOX_combustion.m
+++ Examples/Ex12_LOX_combustion.m
@@ -7,62 +7,54 @@
 % LOX - LH2 combustion
 %
 % Temperature of stoichiometric combustion of H2 O2
-% reactives inlet as satured liquit at 10 bar
+% assuming that the reactives enter inlet the chamber as satured 
+% liquid at 10 bar
 %
-% Requires INIST this example
-% O2 (NIST) hv(404.36 K)-hl(119.62 K)=14.3753 kJ/mol
-% H2 (NIST) hv(413.96K)-hl(31.39K)=10.9495 kJ/mol
+% This example requieres INIST
 
-function [Tp,np]=Ex12_LOX_combustion
 
-clear; clc;
+clear; 
 
 species={'H2','O2' , 'H2O','H','O','OH'};
 nr=[2;1;0;0;0;0]'; % mol
 P=10;
 
-% Enthalpy of liquid O2 at Tsat 10 bar (kJ/mol)
-h1_O2 = INIST('O2','h_pt',10,119.62);
-href_O2 = INIST('O2','h_pt',10,404.36);
-hO2= HGSsingle('O2','h',404.36,10); 
+% In INIST we find the enthalpy of O2 at the inlet:
+hO2_INIST=INIST('O2','hl_p',10); % kJ/kg
+% and convert it to kJ/mol
+hO2_INIST=hO2_INIST*HGSsingle('O2','Mm')/1000; % kJ/mol
 
-% Enthalpy of liquid H2 at Tsat 10 bar (kJ/mol)
-h1_H2 = INIST('H2','h_pt',10,31.39);
-href_H2 = INIST('H2','h_pt',10,413.96);
-hH2= HGSsingle('H2','h',413.96,10); 
+% However, this enthalpy is not in the same reference as in HGS
+% BUT, the difference of enthalpies is roughly the same (the difference is
+% due to the fluid model).
+% We choose an arbitrary reference state (10 bar, 300K), accesible to both codes
+hO2_ref_INIST=INIST('O2','h_pt',10,300) * HGSsingle('O2','Mm')/1000; % kJ/mol
 
-% Enthalpy of stoichiometric mixture
-HinLIQ=2*hH2+1*hO2-2*(href_H2-h1_H2)*HGSsingle('H2','Mm')/1000-(href_O2-h1_O2)*HGSsingle('O2','Mm')/1000;
+deltaH_O2=hO2_ref_INIST - hO2_INIST; 
+
+hO2_ref_HGS=HGSsingle('O2','h',300,10);
+
+hO2_inlet_HGS = hO2_ref_HGS - deltaH_O2; % kJ/mol
+
+% We repeat the method for H2
+hH2_INIST=INIST('H2','hl_p',10)*HGSsingle('H2','Mm')/1000; % kJ/mol
+hH2_ref_INIST=INIST('H2','h_pt',10,300) * HGSsingle('H2','Mm')/1000; % kJ/mol
+deltaH_H2=hH2_ref_INIST - hH2_INIST; 
+hH2_ref_HGS=HGSsingle('H2','h',300,10);
+hH2_inlet_HGS = hH2_ref_HGS - deltaH_H2; % kJ/mol
+
+% Finally, we evaluate the TOTAL inlet enthalpy in the HGS reference:
+HinLIQ= 2*hH2_inlet_HGS+1*hO2_inlet_HGS;
 
 
-% HGStp call
-[Tp1,n,species,~] = HGStp(species,nr,'H',HinLIQ,P);
+% HGStp call, with known H at the inlet
+[Tpl,~,~,~] = HGStp(species,nr,'H',HinLIQ,P);
 
-
-% That is what HGStp does
-opt = optimset('Display','off','TolFun',1e-14,'TolX',1e-14);
-Tp=fzero(@(T)DeltaH(T),2500,opt);      % Solver (fsolve is a sh*t and it has a scaling problem so we use fzero), in our cas we use a HGSsecant
-[~,np,~]= HGSeq(species,nr,Tp,P); % After getting Tp, mols equilibrium in a T P
-
-    function DeltaH=DeltaH(Tprod)
-        T = Tprod;
-        % The result must be in thermodynamic equilbrium at the T suposed
-        [~,comp,~] = HGSeq(species,nr,T,P); 
-        
-        % Enthalpy of the combustion at the T suposed
-        Hout = HGSprop(species,comp,T,P,'H');
-        
-        % Has to be 0 for solving. Pre combustion and Post combusiton
-        % enthalpy must be equal
-%         fprintf('T = %.3f, Hout= %e, Hin= %e, hastobe0= %f\n',T,Hout,HinLIQ,Hout-HinLIQ)
-        DeltaH = (Hout-HinLIQ);
-    end
+% HGStp call, with known T at the inlet (gas)
+[Tpg,~,~,~] = HGStp(species,nr,'T',300,P);
 
 
 % Printing results
-fprintf('   HGStp <%f>   DeltaH<%f>\n',Tp1,Tp)
-for ii=1:length(n)
-    fprintf('%s       <%f>             <%f>\n',species{ii},n(ii),np(ii))
-end
+fprintf('HGStp Tpl=%f saturated liquid at 10bar inlet K \n',Tpl);
+fprintf('HGStp Tpg=%f 300K gas inlet K\n',Tpg);
 
-end
