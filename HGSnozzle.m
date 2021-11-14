ebbd430f9846a90427bcaa209567fea9c273b0cf
CalebFuster <calebfuji@gmail.com>
2021-11-14 17:27:22 +0100

    HGSnozzle new function, untracking HGSrocket

--- HGSnozzle.m
+++ HGSnozzle.m
@@ -1,12 +1,12 @@
-function [Tp,species,np,M,F,Isp,flag] = HGSnozzle(species,n0,T0,P0,P1,Pa,A,Fro_Shift,options1,options2)
+function [species,throat,exit,flag] = HGSnozzle(species,n0,T0,P0,Pe,Pa,Fro_Shift,options1,options2)
 %**************************************************************************
 %
-% [Tp,species,np,M,F,Isp,flag] = HGSnozzle(species,n0,T0,P0,P1,Pa,A,
-%                                          Fro_Shift,options1,options2)
+% [species,throat,exit,flag] = HGSnozzle(species,n0,T0,P0,Pe,Pa,Fro_Shift,
+%                                           options1,options2)
 %
 %**************************************************************************
 % 
-% HGSnozzle calculates from the inlet nozzle conditions the outlet 
+% HGSnozzle calculates from the inlet nozzle conditions the thoat and exit 
 % conditions plus the thrust and Isp. 
 %
 %**************************************************************************
@@ -16,9 +16,8 @@ function [Tp,species,np,M,F,Isp,flag] = HGSnozzle(species,n0,T0,P0,P1,Pa,A,Fro_S
 % n0 --> [mols] Number of mols of each species
 % T0 --> [K] Initial temperature
 % P0 --> [bar] Inlet pressure
-% P1 --> [bar] Exit pressure
+% Pe --> [bar] Exit pressure
 % Pa --> [bar] Atmospheric pressure
-% A --> [m^2] Exit area
 % Fro_Shift --> Select between: 'Frozen' for frozen flow
 %                               'Shifting' for shifting flow
 %                               'Combined' for shifting flow until throat
@@ -44,17 +43,24 @@ function [Tp,species,np,M,F,Isp,flag] = HGSnozzle(species,n0,T0,P0,P1,Pa,A,Fro_S
 %
 % Outputs:
 %--------------------------------------------------------------------------
-% Tp --> [K] Exit temperature
-% np --> [mols] Species resultant mols
 % species --> String or code of species
-% M --> [] Exit Mach
-% F --> [N] Thrust
-% Isp --> [s^]Specific impulse, g0 = 9.807 m/s^2
+% throat --> cell contaning
+%           {1} nt --> [mols] Species mols in throat
+%           {2} Tt --> [K] Throat temperature
+%           {3} Pt --> [bar] Throat pressure
+%           {4} At --> [m^2] Throat area
+% exit --> cell contaning
+%           {1} ne --> [mols] Species mols in exit
+%           {2} Te --> [K] Exit temperature
+%           {3} Me --> [] Exit Mch
+%           {4} Ae --> [m^2] Exit area
+%           {5} F --> [N] Thrust
+%           {6} Isp --> [s^]Specific impulse, g0 = 9.807 m/s^2
 % flag --> Solver error detection: 
 %                 1  Solver has reached the solution
 %                -1  Solver failed. Maximum iterations
 %                -2  Solver failed. Initial sign change not found
-%                Only for Fro_shift = 'Combinated'
+%                Only for Fro_shift = 'Combined'
 %                -3  Solver failed. Maximum iterations in P loop
 %                -4  Solver failed. Initial sign change not found in P loop
 %
@@ -84,15 +90,21 @@ end
 [mm] = HGSprop(id,n0,T0,P0,'Mm');
 m=sum(n0)*mm*1e-3;
 
+
+% Throat calculation
+if strcmp(Fro_Shift,'Shifting') || strcmp(Fro_Shift,'Combined')
+    [Tt,~,nt,Pt,flag] = HGSisentropic(id,n0,T0,P0,'Shifting','M',1,options1,options2);
+elseif  strcmp(Fro_Shift,'Frozen')
+    [Tt,~,nt,Pt,flag] = HGSisentropic(id,n0,T0,P0,'Frozen','M',1,options1,options2);
+else
+    error('Your variable Fro_Shift is no one accepted by this function. Only Frozen and Shifting are accepted')
+end
+
+% Exit calculation
 if strcmp(Fro_Shift,'Shifting') || strcmp(Fro_Shift,'Frozen')
     % Shifting and Frozen doesnt require extra action
-    [Tp,~,np,M,flag] = HGSisentropic(id,n0,T0,P0,Fro_Shift,'P',P1,options1);
-    
-elseif strcmp(Fro_Shift,'Combined')
-    % For a case of Shifting until the throat and Frozen for the rest of
-    % the expansion
-    % Throat M=1
-    [Tt,~,nt,Pt,flag] = HGSisentropic(id,n0,T0,P0,'Shifting','M',1,options1,options2);
+    [Te,~,ne,Me,flag] = HGSisentropic(id,n0,T0,P0,Fro_Shift,'P',Pe,options1);   
+else
     if flag ~=1
        return 
     end
@@ -104,9 +116,7 @@ elseif strcmp(Fro_Shift,'Combined')
        return 
     end
     % Frozen expansion
-    [Tp,~,np,M,flag] = HGSisentropic(id,nt,TinitF,P0,'Frozen','P',P1,options1);
-else
-    error('Your variable Fro_Shift is no one accepted by this function. Only Frozen and Shifting are accepted')
+    [Te,~,ne,Me,flag] = HGSisentropic(id,nt,TinitF,P0,'Frozen','P',Pe,options1);
 end
 
 % Flag error return
@@ -114,12 +124,28 @@ if flag ~=1
    return 
 end
 
+% Other properties of the nozzle throat
+[Rg,a,Mm] = HGSprop(id,nt,Tt,Pt,'Rg','a','Mm');
+% Ideal Gas law
+rho = Pt/(Rg*Tt);
+% Continuity
+At = sum(n)/(Mm*a*rho);
+
+
 % Other properties of the nozzle exit
-[a] = HGSprop(id,np,Tp,P1,'a');
-v2=M*a;
+[Rg,a,Mm] = HGSprop(id,nt,Te,Pe,'Rg','a','Mm');
+v2=Me*a;
+% Ideal Gas law
+rho = Pt/(Rg*Tt);
+% Continuity
+Ae = sum(n)/(Mm*v2*rho);
+
+% Properties
 F = m*v2-A*(P1-Pa);
 g0 = 9.807;
 Isp = v2/g0;
 
+throat = {nt, Tt, Pt, At};
+exit = {ne, Te, Me, Ae};
 
 end
