6ccba02eaec5192da02ea809954598434f32552b
CalebFuster <calebfuji@gmail.com>
2021-11-17 11:29:45 +0100

    Example revision,  new numbers and comment of version updatted

---
+++ Examples/Ex08_H2_O2_reaction_enthalpy_change.m
@@ -0,0 +1,42 @@
+%***********************************************************************************************************
+% *HGS 2.0
+% *By Caleb Fuster, Manel Soria and Arnau Miró
+% *ESEIAAT UPC    
+%***********************************************************************************************************
+%
+% Adiabatic H2 / O2 reaction with dissociation
+% (as an example of how HPStp looks for the temperature)
+
+% Inlet: H2, O2 
+% Outlet: H2O + (1/2)O2 at Tp
+
+clear
+close 
+
+species={'H2','O2','H2O','H','O','OH'};
+Tr=350; % K 
+P=66; % bar
+nr=[2;1;0;0;0;0]; % mol
+ 
+Hin = HGSprop(species,nr,Tr,P,'H');
+ 
+T=linspace(300,5000,20);
+for i=1:length(T) 
+    [~,comp,~]= HGSeq(species,nr,T(i),P);
+    Hout(i) = HGSprop(species,comp,T(i),P,'H');    
+end
+ 
+plot(T,Hout,'-r',T,Hin*ones(length(T),1),'-b','LineWidth',2)
+hold on
+
+
+[Tp,~,np,~] = HGStp(species,nr,'T',Tr,P)
+np/sum(np)
+flameHout = HGSprop(species,np,Tp,P,'H');
+plot(Tp,flameHout,'ok','MarkerSize',14,'MarkerFaceColor','k');
+
+set(gca,'FontSize',18)
+xlabel('T');
+ylabel('H');
+legend({'Hout','Hin','HGStp reaction temperature'});
+title('H2 O2 combustion with dissociation');
---
+++ Examples/Ex10_2019.m
@@ -0,0 +1,73 @@
+%***********************************************************************************************************
+% *HGS 2.1
+% *By Caleb Fuster, Manel Soria and Arnau Miró
+% *ESEIAAT UPC              
+%***********************************************************************************************************
+%
+% Isentropic comparison with RPA software
+
+clear; 
+clc
+
+fprintf('This example has to be revised\n');
+return
+
+
+species={'H','H2','H2O','H2O2','HO2','O','O2','O3','OH'}
+
+% ratio OF = 8 : mO2 / mH2 = 8 
+% assume mH2 = 1 g : mO2 = 8g
+
+% (8/32 mol O2) / (1/2 mol H2 )  = 0.5 mol O2 / mol H2
+
+%  {'H','H2','H2O','H2O2','HO2','O','O2','O3','OH'}
+n=[  0 , 1  , 0   , 0    , 0   , 0 , 0.5, 0  , 0];
+T=3419.88;
+P=20;
+P2=1;
+format compact
+
+[~,neq,~]= HGSeq(species,n,T,P);
+
+fprintf('Fraction of each species after reaction\n');
+
+neq/sum(neq)
+
+
+dT=0.1;
+
+[MM,Cp,Cv,H,S,~,Rg,gamma,a]= HGSprop(species,neq,T,P);
+
+H/(MM/1000)
+
+S/(MM/1000)
+
+gamma
+
+Rg
+
+a
+
+fprintf('Fraction of each species after isentropic expansion\n');
+
+
+[T2,~,n2,M2,~]= HGSisentropic(species,neq,T,P,'Shifiting','P',P2);
+
+n2/sum(n2)
+
+
+% Nozzle inlet mole fractions (RPA)
+% for O/F ratio = 7.937 
+
+ni_rpa=[ 0.0644043;...  % H
+    0.1402066;...       % H2
+    0.6176198;...       % H2O
+    0.0000024;...       % H2O2
+    0.0000367;...       % HO2
+    0.0268578;...       % O
+    0.0467048;...       % O2
+    0.1041674];         % OH
+    
+Tc=3027.58  % K (RPA)
+Pc=1        % bar (RPA)
+
